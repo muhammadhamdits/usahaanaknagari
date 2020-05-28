7355c6a6e218e72d74fce9d9e0b96eb4e56acc55
nadillaxoloves81 <nadila.syihaq8@gmail.com>
2020-05-28 09:11:20 +0700

    font

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,7 +8,7 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ url('css/all.min.cs') }}">
+    <link rel="stylesheet" href="{{ url('css/all.min.css') }}">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bbootstrap 4 -->
