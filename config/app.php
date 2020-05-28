a86712e0910c4d0df0727436370a2f98edfb4d9b
muhammadhamdits <muhammadhamdi702@gmail.com>
2020-05-28 12:27:54 +0700

    Revert "Menambahkan Maps (Google Maps)"
    This reverts commit e401641d49d19e2fd7c1bb84ae84aae63369f831.

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        FarhanWazir\GoogleMaps\GMapsServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'GMaps' => FarhanWazir\GoogleMaps\Facades\GMapsFacade::class,
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('layout.app');
-// });
-
-Route::get('/', 'DashboardController@map')->name('index');
+Route::get('/', function () {
+    return view('layout.app');
+});
