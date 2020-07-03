44c092da426df57ee780a9a5899f1858ac164ca1
muhammadhamdits <muhammadhamdi702@gmail.com>
2020-07-03 08:50:42 +0700

    lagi

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -185,6 +185,7 @@
 
     window.onload = function() {
         if(!window.location.hash) {
+            window.location = window.location + '#loaded';
             window.location.reload();
         }
     }
