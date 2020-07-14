cc3dfbb92774a84988e5803a6a990d99481c6eef
muhammadhamdits <muhammadhamdi702@gmail.com>
2020-07-14 17:35:38 +0700

    overflow sidebar right

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -89,7 +89,7 @@
             var kanan =
             "<section class='col-3 connectedSortable' id='kanan'>"+
                 "<div class='card'>"+
-                    "<div class='card-body' id='isiKanan' style='height: 465px'>";
+                    "<div class='card-body' id='isiKanan' style='height: 465px; overflow-y: scroll'>";
             $.getJSON("/json/nama/"+nama, function(data) {
                 $.each(data, function(key, data) {
                     kanan += 
